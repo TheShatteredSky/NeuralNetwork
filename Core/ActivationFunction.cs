8e1d16449b6626cc0dca64fd95591e714b1f4b84
Sky <theopierrepoupet@gmail.com>
2025-07-16 20:55:18 +0200

    Refactor

--- Core/ActivationFunction.cs
+++ Core/ActivationFunction.cs
@@ -3,13 +3,13 @@ namespace NeuralNetwork.Core;
 internal static class ActivationFunction
 {
     internal static double Sigmoid(double x) => 1 / (1 + Math.Exp(-x));
-    internal static double Tanh(double x) => Math.Tanh(x);
-    internal static double RElu(double x) => x > 0 ? x : 0;
-    internal static double LeakyRElu(double x) => x > 0 ? x : 0.01 * x;
-    internal static double And(double x) => (x * x - x) / 2;
-    internal static double Nand(double x) => (-x * x + x + 2) / 2;
-    internal static double Or(double x) => (-x * x + 3 * x) / 2;
-    internal static double Nor(double x) => (x * x - 3 * x + 2) / 2;
-    internal static double Ex(double x) => -x * x + 2 * x;
-    internal static double Nex(double x) => x * x - 2 * x + 1;
+    internal static double TanH(double x) => Math.Tanh(x);
+    internal static double ReLU(double x) => x > 0 ? x : 0;
+    internal static double LeakyReLU(double x) => x > 0 ? x : 0.01 * x;
+    internal static double AND(double x) => (x * x - x) / 2;
+    internal static double NAND(double x) => (-x * x + x + 2) / 2;
+    internal static double OR(double x) => (-x * x + 3 * x) / 2;
+    internal static double NOR(double x) => (x * x - 3 * x + 2) / 2;
+    internal static double EX(double x) => -x * x + 2 * x;
+    internal static double NEX(double x) => x * x - 2 * x + 1;
 }
