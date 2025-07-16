327bd04cc2404ec8eb771eadb56d8343c8a5997e
Sky <theopierrepoupet@gmail.com>
2025-07-16 20:52:51 +0200

    Made usings global.

---
+++ Usings.cs
@@ -0,0 +1,8 @@
+global using NeuralNetwork.Core;
+global using NeuralNetwork.Addons;
+global using NeuralNetwork.Optimizers;
+global using System;
+global using System.Globalization;
+global using System.Numerics;
+global using System.Text;
+global using System.Collections.Concurrent;
