3d1940a0d72891d4fca68e26f35f39463b18749c
Sky <theopierrepoupet@gmail.com>
2025-07-19 14:19:09 +0200

    Slight XML Documentation. Removed useless generation method. Changed the loading of scales.

--- Addons/NetworkUtilities.cs
+++ Addons/NetworkUtilities.cs
@@ -1,7 +1,15 @@
 namespace NeuralNetwork.Addons;
 
+/// <summary>
+/// A collection of utility functions.
+/// </summary>
 public static class NetworkUtilities
 {
+    /// <summary>
+    /// Normalizes the given data to a range of 0 to 1.
+    /// Decrepit method, favorize using the scalers.
+    /// </summary>
+    /// <param name="data">The data to normalize.</param>
     public static void NormalizeData(double[][] data)
     {
         if (data.Length == 0) return;
@@ -32,7 +40,15 @@ public static class NetworkUtilities
         }
     }
 
-    public static (double shift, double scale, double deshift)[] GetInputScales(double[][] data, double tMin, double tMax)
+    /// <summary>
+    /// The shifts, scales and de-shifts needed to UNSCALE data down to a specified range.
+    /// </summary>
+    /// <param name="data">The data for which the scales are needed.</param>
+    /// <param name="tMin">The desired minimum of the range.</param>
+    /// <param name="tMax">The desired maximum of the range.</param>
+    /// <returns>The shift, scale and de-shift for each column of data.</returns>
+    //TODO: I have no idea if a better way to unscale/scale data exists, this is the way I found but frankly seems inefficient.
+    public static (double shift, double scale, double deshift)[] GetScales(double[][] data, double tMin, double tMax)
     {
         int rows = data.Length;
         int cols = data[0].Length;
@@ -51,35 +67,6 @@ public static class NetworkUtilities
             scales[j].deshift = tMin + (tMax - tMin) / 2;
         }
         return scales;
-    }
-
-    public static (double shift, double scale, double descale)[] GetOutputScales(double[][] data, double tMin, double tMax)
-    {
-        int rows = data.Length;
-        int cols = data[0].Length;
-        (double shift, double scale, double deshift)[] scales = new (double, double, double)[cols];
-        for (int j = 0; j < cols; j++)
-        {
-            double min = double.MaxValue;
-            double max = double.MinValue;
-            for (int i = 0; i < rows; i++)
-            {
-                if (data[i][j] < min) min = data[i][j];
-                if (data[i][j] > max) max = data[i][j];
-            }
-            scales[j].shift = -(min + (max - min) / 2 );
-            scales[j].scale = (tMax - tMin) / (max - min);
-            scales[j].deshift = tMin + (tMax - tMin) / 2;
-        }
-        return scales;
-    }
-
-    public static void ScaleData(double[][] data, double[] scales)
-    {
-        int columns = scales.Length;
-        for (int column = 0; column < columns; column++)
-            for (int row = 0; row < data.Length; row++)
-                data[row][column] /= scales[column];
     }
     
     public static void SaveToFile(string filePath, Network network) => File.WriteAllText(filePath, network.ToString());
@@ -100,7 +87,7 @@ public static class NetworkUtilities
             if (currentLine >= lines.Length) throw new InvalidDataException("Unexpected end of file while reading layers.");
             string[] layerHeader = lines[currentLine].Split(';');
             if (layerHeader.Length < 2) throw new InvalidDataException($"Invalid layer header at line {currentLine}.");
-            Layer.LayerType layerType = Enum.Parse<Layer.LayerType>(layerHeader[0]);
+            LayerType layerType = Enum.Parse<LayerType>(layerHeader[0]);
             ushort layerSize = ushort.Parse(layerHeader[1]);
             currentLine++;
             Layer layer = new Layer((ushort)layerIdx, layerType);
@@ -131,24 +118,32 @@ public static class NetworkUtilities
                 currentLine++;
             }
         }
-        string[] inScales = lines[currentLine].Split(';'); 
-        string[] outScales = lines[currentLine+1].Split(";");
-        (double shift, double scale, double deshift)[] inputScales = new (double, double, double)[inScales.Length];
-        (double shift, double scale, double deshift)[] outputScales = new (double, double, double)[outScales.Length];
-        for (int i = 0; i < inScales.Length; i++)
+
+        if (lines[currentLine + 1] != "#")
         {
-            string scale = inScales[i];
-            double[] nums = scale.Split(",").Select(x => double.Parse(x, CultureInfo.InvariantCulture)).ToArray();
-            inputScales[i] = (nums[0], nums[1], nums[2]);
+            string[] inScales = lines[currentLine+1].Split(';'); 
+            (double shift, double scale, double deshift)[] inputScales = new (double, double, double)[inScales.Length];
+            for (int i = 0; i < inScales.Length; i++)
+            {
+                string scale = inScales[i];
+                double[] nums = scale.Split(",").Select(x => double.Parse(x, CultureInfo.InvariantCulture)).ToArray();
+                inputScales[i] = (nums[0], nums[1], nums[2]);
+            }
+            network.SetInputScaling(inputScales);
         }
-        for (int i = 0; i < outScales.Length; i++)
+        if (lines[currentLine + 2] != "#")
         {
-            string scale = outScales[i];
-            double[] nums = scale.Split(",").Select(x => double.Parse(x, CultureInfo.InvariantCulture)).ToArray();
-            outputScales[i] = (nums[0], nums[1], nums[2]);
+            string[] outScales = lines[currentLine + 2].Split(";");
+            (double shift, double scale, double deshift)[] outputScales = new (double, double, double)[outScales.Length];
+            for (int i = 0; i < outScales.Length; i++)
+            {
+                string scale = outScales[i];
+                double[] nums = scale.Split(",").Select(x => double.Parse(x, CultureInfo.InvariantCulture)).ToArray();
+                outputScales[i] = (nums[0], nums[1], nums[2]);
+            }
+            network.SetOutputScaling(outputScales);
         }
-        network.SetInputScaling(inputScales);
-        network.SetOutputScaling(outputScales);
+
         return network;
     }
     
@@ -187,17 +182,6 @@ public static class NetworkUtilities
         foreach (var layerBiases in networkBiases)
             for (int n = 0; n < layerBiases.Length; n++)
                 layerBiases[n] = 0;
-    }
-    
-    public static Network GenerateNetwork(Network network)
-    {
-        Network newNet = new Network();
-        newNet.SetName(network.GetName());
-        newNet.Instantiate(network.GetLayerCount() - 2);
-        newNet.CreateInputLayer(network[0].GetSize(), network[0, 0].GetActivation());
-        newNet.CreateHiddenLayers(network[1].GetSize(), network[1, 0].GetActivation());
-        newNet.CreateOutputLayer(network[network.GetLayerCount() - 1].GetSize(), network[network.GetLayerCount() - 1, 0].GetActivation());
-        return newNet;
     }
     
     private static ThreadLocal<Random> _threadRandom = new (() => new Random(BitConverter.ToInt32(Guid.NewGuid().ToByteArray(), 0)));
