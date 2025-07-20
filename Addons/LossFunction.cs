34d8d728fcb8e6ba642016a5e7876b5d78de18d6
Sky <theopierrepoupet@gmail.com>
2025-07-20 04:58:24 +0200

    Just a comment for clarity.

--- Addons/LossFunction.cs
+++ Addons/LossFunction.cs
@@ -5,5 +5,6 @@ internal static class LossFunction
     private static double _epsilon = 1e-16;
     internal static double MSE(double x, double y) => (y - x) * (y - x);
     internal static double BinaryCrossEntropy(double x, double y) => -(y * Math.Log(x + _epsilon) + (1 - y) * Math.Log(1 - x + _epsilon));
+    //I honestly don't really understand why this formula works, but I've checked everywhere, and it's correct so ¯\_(ツ)_/¯
     internal static double CategoricalCrossEntropy(double x, double y) => -(y * Math.Log(x + _epsilon));
 }
